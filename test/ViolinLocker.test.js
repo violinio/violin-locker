3b422bffb7fb7356c3024224521f2b1a595a42f5
M20CR <M20CR@gmail.com>
2021-09-15 20:08:03 +0300

    made withdrawals tests block dependent

--- test/ViolinLocker.test.js
+++ test/ViolinLocker.test.js
@@ -1,6 +1,7 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 const { FakeContract, smock } = require("@defi-wonderland/smock");
+const { BigNumber } = require("ethers");
 
 describe("Locker testing", function () {
   var Locker;
@@ -33,6 +34,11 @@ describe("Locker testing", function () {
 
     expect(await TestToken.balanceOf(owner.address)).to.be.equals(99000000 - 1000000);
     expect(await TestToken.balanceOf(Locker.address)).to.be.equals(1000000);
+
+    //check minted and owner of minted
+    expect(await Locker.balanceOf(owner.address)).to.be.equals(1);
+    expect(await Locker.ownerOf(1)).to.be.equals(owner.address);
+
     await expect(Locker.getLock(1)).to.not.be.reverted;
   });
 
@@ -66,6 +72,9 @@ describe("Locker testing", function () {
     expect(await Locker.connect(owner).createLock(TestToken.address, 1000000, parseInt(aWeek.getTime() / 1000), true, true))
       .to.emit(Locker, "LockCreated")
       .withArgs(2, TestToken.address, owner.address, 1000000);
+    //check minted and owner of minted
+    expect(await Locker.balanceOf(owner.address)).to.be.equals(2);
+    expect(await Locker.ownerOf(2)).to.be.equals(owner.address);
 
     expect(await TestToken.balanceOf(owner.address)).to.be.equals(98000000 - 1000000);
 
@@ -79,18 +88,23 @@ describe("Locker testing", function () {
   });
 
   it("It reverts at withdrawal due to invalid lock", async function () {
-    await expect(Locker.connect(owner).withdraw(3)).be.revertedWith("invalid lock id");
+    await expect(Locker.connect(owner).withdraw(3)).to.be.revertedWith("invalid lock id");
   });
 
   it("It withdraws and checks the balances accordingly", async function () {
-    const aWeek = new Date();
-    aWeek.setDate(aWeek.getDate() + 7);
-    //at some point if it gives you time has passed then try one more time, sometimes we don't match the exact seconds between block and current date
-    expect(
-      await Locker.connect(owner).createLock(TestToken.address, 1000000, parseInt(new Date().getTime() / 1000 + 12), true, true)
-    )
+    const blockNumBefore = await ethers.provider.getBlockNumber();
+    const blockBefore = await ethers.provider.getBlock(blockNumBefore);
+    const timestampBefore = blockBefore.timestamp;
+
+    await network.provider.send("evm_setNextBlockTimestamp", [timestampBefore + 1]);
+    await network.provider.send("evm_mine");
+
+    expect(await Locker.connect(owner).createLock(TestToken.address, 1000000, timestampBefore + 3, true, true))
       .to.emit(Locker, "LockCreated")
       .withArgs(3, TestToken.address, owner.address, 1000000);
+    //check minted and owner of minted
+    expect(await Locker.balanceOf(owner.address)).to.be.equals(3);
+    expect(await Locker.ownerOf(3)).to.be.equals(owner.address);
 
     //decreased the initial supply 4 times
     expect(await TestToken.balanceOf(owner.address)).to.be.equals(96000000);
@@ -101,6 +115,9 @@ describe("Locker testing", function () {
 
     // it's increasing 1 time due to the fact that we withdrew the ammount
     expect(await TestToken.balanceOf(owner.address)).to.be.equals(97000000);
+    //check if burned, so the owner should have -1 at balance
+    expect(await Locker.balanceOf(owner.address)).to.be.equals(2);
+    await expect(Locker.ownerOf(3)).to.be.revertedWith("ERC721: owner query for nonexistent token");
   });
 
   it("It checks for already claimed", async function () {
@@ -113,11 +130,15 @@ describe("Locker testing", function () {
   });
 
   it("It should revert due to not the owner of the lock", async function () {
-    const aWeek = new Date();
-    aWeek.setDate(aWeek.getDate() + 7);
-    //at some point if it gives you time has passed then try one more time, sometimes we don't match the exact seconds between block and current date
+    const blockNumBefore = await ethers.provider.getBlockNumber();
+    const blockBefore = await ethers.provider.getBlock(blockNumBefore);
+    const timestampBefore = blockBefore.timestamp;
+
+    await network.provider.send("evm_setNextBlockTimestamp", [timestampBefore + 1]);
+    await network.provider.send("evm_mine");
+
     expect(
-      await Locker.connect(owner).createLock(TestToken.address, 1000000, parseInt(new Date().getTime() / 1000 + 16), true, true)
+      await Locker.connect(owner).createLock(TestToken.address, 1000000, timestampBefore + 3, true, true)
     )
       .to.emit(Locker, "LockCreated")
       .withArgs(4, TestToken.address, owner.address, 1000000);
